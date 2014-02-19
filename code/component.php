7ed702eb036c161a62aefbfed6ff8ceb8586f5a2
Lu Nguyen Nhat <lu@redweb.dk>
2014-02-19 11:30:13 +0700

    Code style

--- code/component.php
+++ code/component.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package    Template.Function
+ *
+ * @copyright  Copyright (C) 2005 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ *
+ */
+
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 ?>
--- code/index.php
+++ code/index.php
@@ -1,18 +1,19 @@
 <?php
 /**
- * @copyright      Copyright (C) 2013 redComponent
- * @author         redComponent
- * @package        Template
+ * @package    Template.Function
+ *
+ * @copyright  Copyright (C) 2005 - 2014 redCOMPONENT.com. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
  *
  */
 
-// no direct access
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
 // Include the framework
-require(dirname(__FILE__).'/'.'wright'.'/'.'wright.php');
+require dirname(__FILE__) . '/wright/wright.php';
 
 // Initialize the framework and
 $tpl = Wright::getInstance();
-$tpl->addJSScript(JURI::root().'templates/' . $this->template . '/js/js.js');
+$tpl->addJSScript(JURI::root() . 'templates/' . $this->template . '/js/js.js');
 $tpl->display();
